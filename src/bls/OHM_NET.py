efc2023c6c320af6c1c51b5603398e46c5190c36
pooka <code@pookaplay.com>
2024-11-24 09:37:57 -0700

    pre weight refactor

--- src/bls/OHM_NET.py
+++ src/bls/OHM_NET.py
@@ -5,9 +5,19 @@ from bls.OHM_v3 import OHM_v3
 
 class OHM_NET:
     
-    def __init__(self, D=2, Nin = 4, Nout = 5, ptf="") -> None:
+    def __init__(self, param) -> None:
 
-        self.ohm = OHM_v3(D, Nin, Nout, ptf)
+        self.param = param
+        self.numStack = param['numStack']      # number of parallel nodes        
+        self.numInputs = param['numInputs']
+        self.numLayers = param['numLayers']
+
+        self.memD = param['memD']        
+        self.K = param['memK']  
+        
+        self.ohm = [[OHM_v3(self.numInputs, self.K, param) for _ in range(self.numStack)] for _ in range(self.numLayers)] 
+        # D=2, Nin = 4, Nout = 5, ptf="") -> None:
+        #self.ohm = OHM_v3(self.numInputs, self.K, Nout, ptf)
         
     def Reset(self) -> None:
         self.ohm.Reset()        
