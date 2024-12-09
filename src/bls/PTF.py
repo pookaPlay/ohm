834f784f84948b51fbbae7cb5a2649fb012e73d0
pooka <code@pookaplay.com>
2024-12-09 14:09:32 -0700

    PRE ptf refactor

--- src/bls/PTF.py
+++ src/bls/PTF.py
@@ -1,46 +1,71 @@
 import math
+from bls.ADD import ADD
 
 class PTF:
     def __init__(self, D=3) -> None:
         self.D = D
-        self.weights = list(self.D * [1])
-        self.threshold = self.D/2
+        self.local_weights = list(self.D * [1])
+        self.local_threshold = self.D/2
         #self.threshold = self.D
         self.y = 0
         self.lastx = list(self.D * [0])
+        
+        # binary tree implementation
+        self.tree = list()        
+        numStart = int(D/2)                            
+        if numStart > 1:
+            self.tree.append([ADD() for _ in range(numStart)])
+            numStart = int(numStart / 2)
+            while numStart > 1:
+                self.tree.append([ADD() for _ in range(numStart)])
+                numStart = int(numStart / 2)            
+                    
+        self.tree.append([ADD()])                
             
-
-    def SetMin(self) -> None:
-        self.weights = list(self.D * [1])
-        self.threshold = self.D 
-
-    def SetMax(self) -> None:
-        self.weights = list(self.D * [1])
-        self.threshold = 1
-
-    def SetMedMax(self, val=1) -> None:
-        self.weights = list(self.D * [1])
-        self.threshold = self.D/2 - val
-
-    def SetMedian(self) -> None:
-        self.weights = list(self.D * [1])
-        self.threshold = self.D/2
-
     def Output(self) -> int:
         return self.y
 
     def Calc(self, x) -> None:
         
         self.lastx = x.copy()
-        temp = sum([self.weights[i] for i in range(self.D) if x[i] == 1])
-        self.y = 1 if temp >= self.threshold else 0        
+        temp = sum([self.local_weights[i] for i in range(self.D) if x[i] == 1])
+        self.y = 1 if temp >= self.local_threshold else 0        
+
+        
+        #intParam = memParam.GetLSBIntsHack()
+        #threshParam = memThresh.GetLSBIntsHack()                
+        #halfSum = threshParam[0]
+        #self.treeInputs = list(self.numInputs * [0])
+        #for i in range(len(self.inputs)):                                    
+        #    self.treeInputs[i] = self.inputs[i] * intParam[i]        
+        #self.pbfOut = 1 if (sum(self.treeInputs) >= halfSum) else 0     
 
     def Print(self, prefix=""):
         print(f"{prefix} PTF: {self.lastx} -> {self.y}")
 
     def Step(self, x) -> None:
-        self.Calc(x)
+        #self.Calc(x)
         pass
 
     def Reset(self) -> None:
         self.y = 0
+        for ti in range(len(self.tree)):
+            for ai in range(len(self.tree[ti])):
+                self.tree[ti][ai].Reset()
+
+
+    def SetMin(self) -> None:
+        self.local_weights = list(self.D * [1])
+        self.local_threshold = self.D 
+
+    def SetMax(self) -> None:
+        self.local_weights = list(self.D * [1])
+        self.local_threshold = 1
+
+    def SetMedMax(self, val=1) -> None:
+        self.local_weights = list(self.D * [1])
+        self.local_threshold = self.D/2 - val
+
+    def SetMedian(self) -> None:
+        self.local_weights = list(self.D * [1])
+        self.local_threshold = self.D/2
--- src/test_NODE.py
+++ src/test_NODE.py
@@ -26,6 +26,21 @@ def test_NODE():
     input = [random.randint(1, max_value) for _ in range(D)]
     #input = [6, 7, 5]     
     #input = [1, 2, 4]  
+    #input = [25248, 27563, 2654, 16969, 31846, 26538, 19878, 31235, 23466, 14316, 9128, 18471, 9159, 6215, 16418, 9632, 20326, 6473, 4833, 21640, 30943, 6600, 23187, 28454, 20723, 13401, 31262, 29013, 17072, 4082, 921, 6113]   
+    # K=16 D=32
+    # MAX
+    #Data Writer: [-1, 15, -1, 15, -1]
+    #Data Length: [5, 11, 5, 11, 5]
+    # MIN
+    #Data Writer: [0, -16, 0, -16, 0]
+    #Data Length: [5, 11, 5, 11, 5]    
+    # MED
+    #Data Writer: [16, -6, 16, -6, 16]
+    #Data Length: [8, 8, 8, 8, 8]
+    # MEDMAX
+    #Data Writer: [34, -2, 34, -2, 34]
+    #Data Length: [8, 8, 8, 8, 8]
+
     print(f"Input: {input}")
     
     input = [input.copy() for _ in range(10)]
